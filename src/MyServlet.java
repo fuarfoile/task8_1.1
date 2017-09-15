313f337d411b12f6c25aa0c48877eb263c28abdd
Oleh Bondarenko <boa.st@ya.ru>
2017-09-15 23:56:30 +0300

    upd

--- src/MyServlet.java
+++ src/MyServlet.java
@@ -1,9 +1,13 @@
+import model.TextEditor;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Enumeration;
 
 @WebServlet(name = "ServletName", urlPatterns = {"/result"})
 public class MyServlet extends HttpServlet {
@@ -29,6 +33,25 @@ public class MyServlet extends HttpServlet {
     }
 
     public void performTask(HttpServletRequest request, HttpServletResponse response) {
-        RequestOutput.generate(request, response);
+
+        try {
+            String name;
+            ArrayList<String> value = new ArrayList<>();
+
+            Enumeration names = request.getParameterNames();
+            while (names.hasMoreElements()) {
+                name = (String) names.nextElement();
+                value.add(request.getParameterValues(name)[0]);
+            }
+
+            String resultText = TextEditor.getText(Integer.parseInt(value.get(1)));
+
+            request.setAttribute ("fontSize", value.get(0));
+            request.setAttribute ("resultText", resultText);
+            getServletConfig().getServletContext().getRequestDispatcher(
+                    "/jsp/result.jsp").forward(request, response);
+        } catch (IOException | IndexOutOfBoundsException | ServletException e) {
+            e.printStackTrace();
+        }
     }
 }
---
+++ webapps/Task8/jsp/result.jsp
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8" />
+		<title>Task 8</title>
+    </head>
+
+    <body>
+	
+	<h2>Result text :<br> </h2>
+	<pre style="font-size:${fontSize}px">${resultText}</pre>
+        
+    </body>
+
+</html>
